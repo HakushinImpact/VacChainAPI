62c4f78d87534eff4ab6206d7e4c199f96c52ed9
Stick1000 <janleymolina@outlook.com>
2021-08-26 15:58:32 +0800

    Added Block class

---
+++ interfaces/block.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+module.exports = class Block {
+  constructor(index, previousHash, data) {
+    this.index = index;
+    this.hash = null;
+    this.previousHash = previousHash;
+    this.timestamp = Date.now();
+    this.data = data;
+  }
+
+  computeHash() {
+    return crypto
+      .createHash('sha256')
+      .update(
+        this.index +
+          this.previousHash +
+          this.timestamp +
+          JSON.stringify(this.data)
+      )
+      .digest('hex');
+  }
+};

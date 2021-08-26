f36215b06e1dc2d4c45d107a3389c20aee8966cb
Stick1000 <janleymolina@outlook.com>
2021-08-26 16:00:50 +0800

    Added Blockchain class

---
+++ interfaces/blockchain.js
@@ -0,0 +1,51 @@
+const Block = require('./block');
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '0', 'Genesis Block');
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  getBlock(hash) {
+    // Best performance compared to using find()
+    for (let index = 0; index < this.chain.length; index++) {
+      if (this.chain[index].hash === hash) {
+        return this.chain[index];
+      }
+    }
+  }
+
+  getAllBlocks() {
+    return this.chain;
+  }
+
+  addBlock(data) {
+    const newBlock = new Block(
+      this.chain.length,
+      this.getLatestBlock().hash,
+      data
+    );
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 0; i < this.chain.length; i++) {
+      if (this.chain[i].hash !== this.chain[i].computeHash()) {
+        return false;
+      } else if (i > 0 && this.chain[i].prevHash !== this.chain[i - 1].hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+module.exports = new BlockChain();

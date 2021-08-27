8ad089e0393acc52b6ec56d06cd1e2132be1085d
Stick1000 <janleymolina@outlook.com>
2021-08-27 11:24:32 +0800

    Added configuration file for MongoDB

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  database: 'vacchain',
+  collection: 'clients',
+  uri: 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.rdk0a.mongodb.net/vacchain?retryWrites=true&w=majority',
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+};

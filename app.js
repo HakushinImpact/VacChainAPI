fd6965d4523eae8bdc8f6330d72a37810fbafdce
Stick1000 <janleymolina@outlook.com>
2021-08-27 15:53:53 +0800

    Refactored CORS bypass and /update endpoint

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 5000;
 
 const VacChainAPI = require('./interfaces');
 
-app.use(cors());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.post('/insert', async (req, res) => {
   await VacChainAPI.insertEntry(req.body);
   res.status(200).send('OK');
 });
 
-app.get('/update', async (req, res) => {
-  await VacChainAPI.updateEntry(req.query.id);
+app.post('/update', async (req, res) => {
+  await VacChainAPI.updateEntry(req.body.id, req.body.data);
   res.status(200).send('OK');
 });
 

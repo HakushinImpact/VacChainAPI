7eacbeee4af1345bf0bc446143735b13f6ca7007
Stick1000 <janleymolina@outlook.com>
2022-04-24 23:22:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   database: 'vacchain',
   collection: 'clients',
-  uri: 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.rdk0a.mongodb.net/vacchain?retryWrites=true&w=majority',
+  uri: 'mongodb+srv://m001-student:m001-mongodb-basics@dummy.uxerc.mongodb.net/vacchain?retryWrites=true&w=majority',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
